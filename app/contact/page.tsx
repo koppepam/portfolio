a59a9beb3d4ed2c1f49576a7e616f1f08371833b
riseyoneyama <rise.yoneyama@gmail.com>
2024-06-02 17:08:07 +0900

    お問い合わせフォームのページ

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,22 +1,57 @@
-export default function Contact() {
+"use client";
+
+import { FormEvent } from "react";
+
+export default async function Contact() {
+  const sendContent = (event: FormEvent) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form as HTMLFormElement);
+    fetch(`https://script.google.com/macros/s/AKfycbw2UI0puuKQ56L8zcIjkeOaYEV7Ox_7v1fZkBWytYJ3Wx1sFOWxO7i3RGZLd9L6_9VF/exec`, {
+      mode: 'no-cors',
+      method: 'POST',
+      headers: {
+        // Accept: 'application/json',
+        'Content-type': 'application/x-www-form-urlencoded',
+        // 'Access-Control-Allow-Credentials': 'true',
+      },
+      body: JSON.stringify(formData),
+    }).then((response) => {
+      console.log(response.status);
+      // リダイレクト
+      if (response.status === 200) { // 成功は302?
+        alert('お問い合わせありがとうございました！');
+        console.log(formData);
+        location.href = '/';
+      } else if (response.status === 400) {
+        alert('送信ができませんでした。');
+        location.href = '/';
+      }
+    });
+  };
+
   return (
-    <main>
-      <h1>お問い合わせフォーム</h1>
+    <main className="m-24">
+      <h1 className="text-lg mb-10">お問い合わせフォーム</h1>
       <div>
-          <form id="contactForm" action="">
+          <form id="contactForm" onSubmit={sendContent} >
             <div>
-              <textarea name="name" id="name" rows={1} placeholder="お名前を入力してください"></textarea>
-              <input id="name" type="text"></input>
+              お名前
+              <input className="bg-gray-200 appearance-none border-2 border-gray-400 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                type="text" name="name" id="name" placeholder="お名前を入力してください" />
             </div>
             <div>
-              <textarea name="adress" id="adress" rows={1} placeholder="メールアドレスを入力してください"></textarea>
-              <input id="adress" type="text"></input>
+              メールアドレス
+              <input className="bg-gray-200 appearance-none border-2 border-gray-400 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                type="email" name="email" id="email" placeholder="メールアドレスを入力してください" />
             </div>
             <div>
-              <textarea name="content" id="content" rows={10} placeholder="お問い合わせ内容を入力してください"></textarea>
-              <input id="content" type="text"></input>
+              お問い合わせ内容
+              <textarea className="bg-gray-200 appearance-none border-2 border-gray-400 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                name="content" id="content" rows={10} placeholder="お問い合わせ内容を入力してください" ></textarea>
             </div>
-            <button type="submit">送信する</button>
+            <button className="rounded-2xl bg-sky-400 hover:bg-sky-600 text-white p-3 mr-3"
+              type="submit" >送信する</button>
           </form>
         </div>
     </main>
