35bf63c894af4bb5a9c176f58d5f41756004fe6a
riseyoneyama <rise.yoneyama@gmail.com>
2024-06-04 20:49:14 +0900

    FadeInアニメーション作成

---
+++ components/FadeIn.jsx
@@ -0,0 +1,20 @@
+export const FadeIn = () => {
+  const callback = function (entries) {
+    entries.forEach((entry) => {
+      //console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate-slideIn");
+      } else {
+        entry.target.classList.remove("animate-slideIn");
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback);
+
+  const targets = document.querySelectorAll(".js-show-on-scroll");
+  targets.forEach(function (target) {
+    target.classList.add("opacity-0");
+    observer.observe(target);
+  });
+};
